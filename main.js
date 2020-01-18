38579e19fd68460ed05a3e02ab7c4851c0e8634b
Igor Rodionov <henryshaw217@gmail.com>
2020-01-18 12:57:00 +0300

    повесил событие на кнопку

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const header = document.querySelector('.header');
 const list = document.querySelector('.pages-list');
 const links = Array.from(document.querySelectorAll('.pages-item__link'));
 const signUpBtns = Array.from(document.querySelectorAll('.service-link'));
+const signUp = document.querySelector('.header-service');
 
 const showcaseSection = document.querySelector('.showcase');
 const serviceSection = document.querySelector('.service');
@@ -58,50 +59,18 @@ signUpBtns.forEach(el => {
   })
 })
 
-
-
-function closeDiv(openedElem) {
-    if (openedElem && openedElem.style.display == 'block') {
-        openedElem.style.display = 'none';
+  signUp.addEventListener('click', event => {
+    event.preventDefault();
+    let top;
+    if (event.target.dataset.id) {
+      top = contactsSection.offsetTop;
     }
-};
-
-function top_walker (node, test_func, last_parent) {
-	while ( node && node !== last_parent ) {
-		if ( test_func(node) ) {
-			return node;
-		}
-		node = node.parentNode;
-	}
-}
-
-document.documentElement.addEventListener('click', (event) => {
-  if (event.target.classList.contains('bars-button') || event.target.parentElement.classList.contains('bars-button')) {
-    bars.classList.toggle('hidden');
-  } else {
-    let isClickWithinOpenedDiv = top_walker(event.target, function (node) {
-        return node === bars
-      });
-
-    if (!isClickWithinOpenedDiv) {
-       bars.classList.add('hidden');
-    } else {
-      return;
-    }
-  }
-})
-
-
-let sticky = header.offsetTop;
-header.classList.remove('sticky');
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > sticky) {
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove('sticky');
-  }
-});
+  
+    window.scrollTo({
+      top: top,
+      behavior: 'smooth'
+    })
+  })
 
 
 window.addEventListener('resize', () => {
